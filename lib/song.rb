b408be50228cb716c172f45b0b5102b5eb505d84
github username <github email address>
2019-04-05 15:13:50 -0400

    finished

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,13 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :create
   @@all = []
+
+  def initialize(name,artist_name=nil)
+    @name = name
+    @artist_name = artist_name
+    #binding.pry
+    @@all << self
+  end
 
   def self.all
     @@all
@@ -10,4 +17,63 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    Song.new(name) 
+  end
+
+  def self.new_by_name(name) 
+    Song.new(name)
+    #binding.pry
+  end
+
+  def self.create_by_name(name)
+    Song.new(name)
+  end
+
+  def self.find_by_name(song_name)
+    all.find do |songs|
+      #binding.pry
+      songs.name == song_name
+      
+    end
+    #binding.pry
+  end
+
+  def self.find_or_create_by_name(what)
+    
+    create_by_name(what)
+    find_by_name(what)
+    
+  end
+
+  def self.alphabetical
+    #all[0].name #returns name of instance 
+    #binding.pry
+    new_array = all.sort_by do |instances|
+      #binding.pry
+      instances.name
+    end
+  end
+
+  def self.new_from_filename(string)
+    string = string.split(" - ")
+    #@artist_name = string[0]
+    #@name = string[1]
+    Song.new(string[1].chomp(".mp3"),string[0])
+    #binding.pry
+  end
+  
+  def self.create_from_filename(string)
+    string = string.split(" - ")
+    #@artist_name = string[0]
+    #@name = string[1]
+    Song.new(string[1].chomp(".mp3"),string[0])
+    #binding.pry
+  end
+
+  def self.destroy_all
+     @@all = []
+  end
 end
+
+
